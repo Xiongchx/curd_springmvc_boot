16eb67460aab65893543755df3319132761175a7
xiongchx <loveryuchi@vip.qq.com>
2019-08-19 19:38:08 +0800

    基本环境搭建

---
+++ src/main/webapp/java/com/boot/curd/Application.java
@@ -0,0 +1,26 @@
+/**
+ * FileName: Application
+ * Author:   26917
+ * Date:     2019/8/19 17:23
+ * Description: 启动类
+ * History:
+ */
+package com.boot.curd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 〈一句话功能简述〉<br>
+ * 〈启动类〉
+ *
+ * @author 26917
+ * @create 2019/8/19
+ * @since 1.0.0
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/main/webapp/resources/templates/user.ftl
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Spring Boot</title>
+    <meta charset="UTF-8"/>
+</head>
+<body>
+${tip}
+</body>
+</html>
