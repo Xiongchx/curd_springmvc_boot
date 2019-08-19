58ac43527657ee323485d5a4c0291ce3896f8633
xiongchx <loveryuchi@vip.qq.com>
2019-08-20 01:03:59 +0800

    完成主界面分页查询

---
+++ src/main/webapp/java/com/boot/curd/controller/EmployeeController.java
@@ -0,0 +1,47 @@
+/**
+ * FileName: EmployeeController
+ * Author:   26917
+ * Date:     2019/8/19 22:54
+ * Description: controller
+ * History:
+ */
+package com.boot.curd.controller;
+
+import com.boot.curd.bean.Employee;
+import com.boot.curd.service.EmployeeService;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈controller〉
+ *
+ * @author 26917
+ * @create 2019/8/19
+ * @since 1.0.0
+ */
+@Controller
+public class EmployeeController {
+
+    @Autowired
+    EmployeeService employeeService;
+
+    @GetMapping("/emps")
+    public String getEmps(@RequestParam(value = "pn",defaultValue = "1")Integer pn, Model model){
+        //引入分页插件，startPage其后紧跟的查询为分页查询
+        PageHelper.startPage(pn,5);
+        List<Employee> emps = employeeService.getAll();
+        // 包装查询结果，由PageInfo封装详细的分页信息，然后将PageInfo交给页面处理
+        // 构造器中的第二个参数为连续显示的页数
+        PageInfo page=new PageInfo(emps,5);
+        model.addAttribute("pageInfo",page);
+        return "list";
+    }
+}
